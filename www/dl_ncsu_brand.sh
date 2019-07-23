9943509837664ef65e467579300735b018873a8e
pdabacus <dayalpavan@gmail.com>
2019-07-23 02:16:55 -0400

    template ncsu page

---
+++ www/dl_ncsu_brand.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo dl bootstrap css
+curl -Lo ncsu-bootstrap.min.css "https://cdn.ncsu.edu/brand-assets/bootstrap/css/bootstrap.css"
+
+echo dl bootstrap js
+curl -Lo ncsu-bootstrap.min.js "https://cdn.ncsu.edu/brand-assets/bootstrap/js/bootstrap.min.js"
+
+echo dl ncsu fonts
+curl -Lo ncsu-font.css "https://cdn.ncsu.edu/brand-assets/fonts/include.css"
+
+echo dl ncsu util bar
+curl -Lo ncsu-utilbar.min.js "https://cdn.ncsu.edu/brand-assets/utility-bar/ub.php?placeholder=search+shit+here&maxWidth=1100&color=gray&showBrick=1"
+sed -i "s/ub\.php/ncsu-utilbar\.min\.js/" ncsu-utilbar.min.js
